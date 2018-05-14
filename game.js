fa5d303fce0edad236a418a7758f2da4bbca1a5c
Patrik <patrikhorsky1@gmail.com>
2018-05-14 15:00:35 +0200

    Added UI(Buttons and score board) updated game mechanics

---
+++ game.js
@@ -0,0 +1,99 @@
+let computerSelection;
+let playerScore = 0;
+let computerScore = 0;
+
+const keepScore = document.getElementById('Score');
+const gameLog1 = document.getElementById('gameLog1');
+const gameLog2 = document.getElementById('gameLog2');
+const outcome = document.getElementById('outcome');
+const body = document.getElementById('body');
+
+function computerPlay(){
+  let random = Math.floor(Math.random()*3);
+  computerSelection = (random == 0)? "Rock" : (random == 1)? "Paper" : "Scissors";
+  return computerSelection;
+}
+
+//One Round of the game - Returns the outcome
+function Round(playerSelection,computerSelection){
+  playerSelection = playerSelection.toLowerCase();
+  playerSelection = playerSelection.charAt(0).toUpperCase()+playerSelection.slice(1);
+  playerPc = playerSelection + computerSelection;
+
+  switch (playerPc) {
+    case "RockRock":
+    case "PaperPaper":
+    case "ScissorsScissors":
+    body.style.background="#d4d6d4";
+      return "It is a Tie!";
+      break;
+
+    case "RockScissors":
+    case "ScissorsPaper":
+    case "PaperRock":
+      playerScore++;
+      body.style.background= "#4ea76a";
+      return "Player Wins!";
+      break;
+
+    case "ScissorsRock":
+    case "PaperScissors":
+    case "RockPaper":
+      computerScore++;
+      body.style.background="#d8635f";
+      return "Computer Wins!";
+      break;
+
+    default:
+      return "Error";
+      break;
+    }
+}
+
+//Runs Round function until one side wins and displays the out come + the score
+function game(playerSelection){
+    let RoundOutCome = Round(playerSelection,computerPlay());
+    console.log(RoundOutCome);
+
+    if (RoundOutCome!="Error") {
+      //let txt = "Player played: "+playerSelection+" Computer played: "+computerSelection;
+      //let txt = "Player played: "+playerSelection+" Computer played: "+computerSelection;
+      console.log("Score Player: "+playerScore+" | Computer: "+computerScore);
+      keepScore.innerHTML=`${playerScore}:${computerScore}`;
+      //Player: *** | Computer: ***
+      gameLog1.innerHTML=`Player: *${playerSelection}* | Computer: *${computerSelection}*`;
+      //gameLog1.innerHTML=`Player: ${playerSelection}`;
+      //gameLog2.innerHTML=`Computer: ${computerSelection}`;
+      outcome.innerHTML=RoundOutCome;
+
+
+  }
+
+  if (playerScore == 5){
+    outcome.innerHTML="*Player Wins The Game*";
+    gameReset();
+  }
+  if(computerScore == 5){
+    outcome.innerHTML="*Computer Wins The Game*";
+    gameReset();
+  }
+}
+
+function gameReset(){
+  playerScore=0;
+  computerScore=0;
+  keepScore.innerHTML=`${playerScore}:${computerScore}`;
+}
+
+let rockbtn = document.querySelector("#rock");
+rockbtn.addEventListener("click", () => {
+  game("Rock");
+});
+let paperbtn = document.querySelector("#paper");
+paperbtn.addEventListener("click", () =>{
+  game("Paper");
+});
+let scissorsbtn = document.querySelector("#scissors");
+scissorsbtn.addEventListener("click", () =>{
+  game("Scissors");
+});
